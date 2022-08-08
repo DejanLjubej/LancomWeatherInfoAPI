81e9def2972e36aa49e722a9c92e299d36a506bd
Dejan Ljubej <dejan.ljubej@gmail.com>
2022-08-08 19:57:23 +0200

    Added DbContext, Connection string in appsettings and added configuration in Startup Class

---
+++ Data/WeatherInfoContext.cs
@@ -0,0 +1,16 @@
+﻿using LanconWeatherAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LancomWeatherInfoAPI.Data
+{
+    public class WeatherInfoContext:DbContext
+    {
+        public WeatherInfoContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; }
+    }
+}
