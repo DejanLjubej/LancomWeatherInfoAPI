aada0885b6586277fc3fb6338343e9cd1c6ba6d3
dejan <dejan.ljubej@gmail.com>
2022-08-08 19:23:15 +0200

    Add project files.

---
+++ Models/City.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanconWeatherAPI.Models
+{
+    public class City
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        // Foreign Key
+        public int CountryId { get; set; }
+    }
+}
---
+++ Models/Country.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanconWeatherAPI.Models
+{
+    public class Country
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        public ICollection<City> Cities { get; set; }
+    }
+}
